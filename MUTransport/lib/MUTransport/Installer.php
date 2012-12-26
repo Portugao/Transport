10d93610591ccf2c03432ad8d13271849bfb2244
Michael Ueberschaer <kontakt@webdesign-in-bremen.com>
2012-12-26 17:03:21 +0100

    typo

--- MUTransport/lib/MUTransport/Installer.php
+++ MUTransport/lib/MUTransport/Installer.php
@@ -13,286 +13,207 @@
 
 /*
  * generated at Sat Dec 12 18:12:57 CET 2009 by ModuleStudio 0.4.3 (http://modulestudio.de)
- */
- 
+*/
+
 class MUTransport_Installer extends Zikula_AbstractInstaller
 {
 
-/**
- * intall the MUTransport module
- *
- * This function is only ever called once during the lifetime of a particular
- * module instance
- * This function MUST exist in the pninit file for a module
- *
- * @author       Michael Ueberschaer
- * @return       bool       true on success, false otherwise
- */
+	/**
+	 * intall the MUTransport module
+	 *
+	 * This function is only ever called once during the lifetime of a particular
+	 * module instance
+	 * This function MUST exist in the pninit file for a module
+	 *
+	 * @author       Michael Ueberschaer
+	 * @return       bool       true on success, false otherwise
+	 */
 
 
-public function install()
-{
-    // create the modul table
-    if (!DBUtil::createTable('mutransport_modul')) {
-        return false;
-    }
-    // create the page table
-    if (!DBUtil::createTable('mutransport_page')) {
-        return false;
-    }
-    // create the cms table
-    if (!DBUtil::createTable('mutransport_cms')) {
-        return false;
-    }
-    // create the cmscontent table
-    if (!DBUtil::createTable('mutransport_cmscontent')) {
-        return false;
-    }
-    // create the user table
-    if (!DBUtil::createTable('mutransport_user')) {
-        return false;
-    }
-    
-    // set up all our module vars with initial values
-    
-    $this->setVar('newstocontent', 0);
-    $this->setVar('pagestocontent', 0);
-    $this->setVar('pagedtocontent', 0);
-    $this->setVar('pagedtonews', 0);
-    $this->setVar('reviewstocontent', 0);
-    $this->setVar('details', 0);    
-    $this->setVar('contenttocontent', 0);
-    $this->setVar('image_path', '');
-    $this->setVar('text_format', 'text');
-    $this->setVar('news_state', 4);
-    $this->setVar('wordpress', 0);
-    $this->setVar('wordpress_host', '');
-    $this->setVar('wordpress_db', '');
-    $this->setVar('wordpress_user', '');
-    $this->setVar('wordpress_pw', '');
-    $this->setVar('wordpress_prefix', '');
-    $this->setVar('image_path2', '');
-    $this->setVar('wordpress_ezcomments', 0);
-    $this->setVar('wordpress_clearing', 0);
+	public function install()
+	{
+		// create the modul table
+		if (!DBUtil::createTable('mutransport_modul')) {
+			return false;
+		}
+		// create the page table
+		if (!DBUtil::createTable('mutransport_page')) {
+			return false;
+		}
+		// create the cms table
+		if (!DBUtil::createTable('mutransport_cms')) {
+			return false;
+		}
+		// create the cmscontent table
+		if (!DBUtil::createTable('mutransport_cmscontent')) {
+			return false;
+		}
+		// create the user table
+		if (!DBUtil::createTable('mutransport_user')) {
+			return false;
+		}
 
-    // create the default data for MUTransport
-    //$this->defaultdata(); maybe we can user later
-    
-    // jump to interactive installation
-    // $this->init_interactiveinit(); 
+		// set up all our module vars with initial values
 
-    // Initialisation successful
-    return true;
-}
+		$this->setVar('newstocontent', 0);
+		$this->setVar('pagestocontent', 0);
+		$this->setVar('pagedtocontent', 0);
+		$this->setVar('pagedtonews', 0);
+		$this->setVar('reviewstocontent', 0);
+		$this->setVar('details', 0);
+		$this->setVar('contenttocontent', 0);
+		$this->setVar('image_path', '');
+		$this->setVar('text_format', 'text');
+		$this->setVar('news_state', 4);
+		$this->setVar('wordpress', 0);
+		$this->setVar('wordpress_host', '');
+		$this->setVar('wordpress_db', '');
+		$this->setVar('wordpress_user', '');
+		$this->setVar('wordpress_pw', '');
+		$this->setVar('wordpress_prefix', '');
+		$this->setVar('image_path2', '');
+		$this->setVar('wordpress_ezcomments', 0);
+		$this->setVar('wordpress_clearing', 0);
 
-/**
- * upgrade the MUTransport module from an old version
- *
- * This function can be called multiple times
- * This function MUST exist in the pninit file for a module
- *
- * @author       Michael Ueberschaer
- * @param       int        $oldversion version to upgrade from
- * @return      bool       true on success, false otherwise
- */
-public function upgrade($oldversion)
-{
+		// create the default data for MUTransport
+		//$this->defaultdata(); maybe we can user later
 
-    // Upgrade dependent on old version number
-    switch ($oldversion){
-    case '1.0':
-    // set new modvars
-    $this->setVar('newstocontent', 0);
-    $this->setVar('pagestocontent', 1);
-    $this->setVar('pagedtocontent', 0);   
-    $this->setVar('contenttocontent', 1);
-    $this->setVar('image_path', '');
-    $this->setVar('text_format', 'text');
+		// jump to interactive installation
+		// $this->init_interactiveinit();
 
-    
-    case '1.2':
-    // set new modvar
-    $this->setVar('pagedtonews', 0);   
-    $this->setVar('news_state', 4);
-    
-    case '1.2.5':
-    // set new modvar
-    $this->setVar('wordpress', 0);
-    $this->setVar('wordpress_db', '');
-    $this->setVar('wordpress_prefix', '');
-    $this->setVar('image_path2', '');
-    $this->setVar('wordpress_ezcomments', 0);
-    $this->setVar('reviewstocontent', 0);
-    $this->setVar('details', 0);
-    $this->setVar('wordpress_clearing', 0);
-    // create the cms table
-    if (!DBUtil::createTable('mutransport_cms')) {
-        return false;
-    }
-    // create the cmscontent table
-    if (!DBUtil::createTable('mutransport_cmscontent')) {
-        return false;
-    }
-    // create the user table
-    if (!DBUtil::createTable('mutransport_user')) {
-        return false;
-    }
-    
-    case '1.3.0':
-    // set new modvar
+		// Initialisation successful
+		return true;
+	}
 
-	$this->setVar('wordpress_host','');
-    $this->setVar('wordpress_user', '');
-    $this->setVar('wordpress_pw', '');
-                 
-    }
+	/**
+	 * upgrade the MUTransport module from an old version
+	 *
+	 * This function can be called multiple times
+	 * This function MUST exist in the pninit file for a module
+	 *
+	 * @author       Michael Ueberschaer
+	 * @param       int        $oldversion version to upgrade from
+	 * @return      bool       true on success, false otherwise
+	 */
+	public function upgrade($oldversion)
+	{
 
-    // Update successful
-    return true;
-}
-
-/**
- * delete the MUTransport module
- * This function is only ever called once during the lifetime of a particular
- * module instance
- * This function MUST exist in the pninit file for a module
- *
- * @author       Michael Ueberschaer
- * @return       bool       true on success, false otherwise
- */
-public function uninstall()
-{
-    if (!DBUtil::dropTable('mutransport_modul')) {
-        return false;
-    }
-    if (!DBUtil::dropTable('mutransport_page')) {
-        return false;
-    }
-
-    if (!DBUtil::dropTable('mutransport_cms')) {
-        return false;
-    }
-    
-    if (!DBUtil::dropTable('mutransport_cmscontent')) {
-        return false;
-    }
-    
-    if (!DBUtil::dropTable('mutransport_user')) {
-        return false;
-    }
+		// Upgrade dependent on old version number
+		switch ($oldversion){
+			case '1.0':
+				// set new modvars
+				$this->setVar('newstocontent', 0);
+				$this->setVar('pagestocontent', 1);
+				$this->setVar('pagedtocontent', 0);
+				$this->setVar('contenttocontent', 1);
+				$this->setVar('image_path', '');
+				$this->setVar('text_format', 'text');
 
 
+			case '1.2':
+				// set new modvar
+				$this->setVar('pagedtonews', 0);
+				$this->setVar('news_state', 4);
 
-    // remove all module vars
-    $this->delVars();
+			case '1.2.5':
+				// set new modvar
+				$this->setVar('wordpress', 0);
+				$this->setVar('wordpress_db', '');
+				$this->setVar('wordpress_prefix', '');
+				$this->setVar('image_path2', '');
+				$this->setVar('wordpress_ezcomments', 0);
+				$this->setVar('reviewstocontent', 0);
+				$this->setVar('details', 0);
+				$this->setVar('wordpress_clearing', 0);
+				// create the cms table
+				if (!DBUtil::createTable('mutransport_cms')) {
+					return false;
+				}
+				// create the cmscontent table
+				if (!DBUtil::createTable('mutransport_cmscontent')) {
+					return false;
+				}
+				// create the user table
+				if (!DBUtil::createTable('mutransport_user')) {
+					return false;
+				}
 
-    // Deletion successful
-    return true;
-}
+			case '1.3.0':
+				// set new modvar
 
-/**
- * create the default data for MUTransport
- *
- * This function is only ever called once during the lifetime of a particular
- * module instance
- *
- * @author       Michael Ueberschaer
- * @return       bool       true on success, false otherwise
- */
-public function defaultdata()
-{
-    // ensure that tables are cleared
-    if (
-        !DBUtil::deleteWhere('mutransport_modul', '1=1') || 
-        !DBUtil::deleteWhere('mutransport_page', '1=1') ||
-        !DBUtil::deleteWhere('mutransport_cms', '1=1')||
-        !DBUtil::deleteWhere('mutransport_cmscontent', '1=1')||
-        !DBUtil::deleteWhere('mutransport_user', '1=1')) {
-        return false;
-    }
+				$this->setVar('wordpress_host','');
+				$this->setVar('wordpress_user', '');
+				$this->setVar('wordpress_pw', '');
+				 
+		}
 
+		// Update successful
+		return true;
+	}
 
-    // insertion successful
-    return true;
-}
+	/**
+	 * delete the MUTransport module
+	 * This function is only ever called once during the lifetime of a particular
+	 * module instance
+	 * This function MUST exist in the pninit file for a module
+	 *
+	 * @author       Michael Ueberschaer
+	 * @return       bool       true on success, false otherwise
+	 */
+	public function uninstall()
+	{
+		if (!DBUtil::dropTable('mutransport_modul')) {
+			return false;
+		}
+		if (!DBUtil::dropTable('mutransport_page')) {
+			return false;
+		}
 
-/**
- * interactive installation procedure
- *
- * @author       Michael Ueberschaer
- * @return       pnRender output
- *//*
-public function init_interactiveinit()
-{
-    if (!SecurityUtil::checkPermission('::', '::', ACCESS_ADMIN)) {
-        return LogUtil::registerPermissionError();
-    }
-    
+		if (!DBUtil::dropTable('mutransport_cms')) {
+			return false;
+		}
 
-    $render = pnRender::getInstance('MUTransport', false);
-    return $render->fetch('MUTransport_init_interactive.htm');
- //   return $this->view->fetch('MUTransport_init_interactive.htm');
-} 
+		if (!DBUtil::dropTable('mutransport_cmscontent')) {
+			return false;
+		}
+
+		if (!DBUtil::dropTable('mutransport_user')) {
+			return false;
+		}
 
 
-*//**
- * interactive installation procedure step 3
- *
- * @author       Michael Ueberschaer
- * @return       pnRender output
- *//*
-public function init_interactiveinitstep3()
-{
-    if (!SecurityUtil::checkPermission('::', '::', ACCESS_ADMIN)) {
-        return LogUtil::registerPermissionError();
-    }
-    
-//    $dom = ZLanguage::getModuleDomain('MUTransport');
-    
-    $activate = (bool) FormUtil::getPassedValue('activate', false, 'POST');
 
-    $render = pnRender::getInstance('MUTransport', false);
-    $render->assign('authid', SecurityUtil::generateAuthKey('Modules'));
-    $render->assign('activate', $activate);
-    return $render->fetch('MUTransport_init_step3.htm');
-}*/
+		// remove all module vars
+		$this->delVars();
 
-/**
- * interactive update procedure
- *
- * @author       Michael Ueberschaer
- * @return       pnRender output
- 
-public function init_interactiveupdate()
-{
-    if (!SecurityUtil::checkPermission('::', '::', ACCESS_ADMIN)) {
-        return LogUtil::registerPermissionError();
-    }
+		// Deletion successful
+		return true;
+	}
 
-//    $dom = ZLanguage::getModuleDomain('MUTransport');
+	/**
+	 * create the default data for MUTransport
+	 *
+	 * This function is only ever called once during the lifetime of a particular
+	 * module instance
+	 *
+	 * @author       Michael Ueberschaer
+	 * @return       bool       true on success, false otherwise
+	 */
+	public function defaultdata()
+	{
+		// ensure that tables are cleared
+		if (
+				!DBUtil::deleteWhere('mutransport_modul', '1=1') ||
+				!DBUtil::deleteWhere('mutransport_page', '1=1') ||
+				!DBUtil::deleteWhere('mutransport_cms', '1=1')||
+				!DBUtil::deleteWhere('mutransport_cmscontent', '1=1')||
+				!DBUtil::deleteWhere('mutransport_user', '1=1')) {
+			return false;
+		}
 
-    // TODO
 
-    return true;
-}
-
-/**
- * interactive delete
- *
- * @author       Michael Ueberschaer
- * @return       pnRender output
-
-public function init_interactivedelete()
-{
-    if (!SecurityUtil::checkPermission('::', '::', ACCESS_ADMIN)) {
-        return LogUtil::registerPermissionError();
-    }
-    
-//    $dom = ZLanguage::getModuleDomain('MUTransport');
-
-    $render = pnRender::getInstance('MUTransport');
-    $render->assign('authid', SecurityUtil::generateAuthKey('Modules'));
-    return $render->fetch('MUTransport_init_delete.htm');
-}*/
+		// insertion successful
+		return true;
+	}
 }
 
