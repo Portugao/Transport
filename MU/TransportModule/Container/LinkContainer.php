5055135b05c8d86bd545de970c62c4faa34e8fdd
Portugao <info@homepages-mit-zikula.de>
2017-08-07 01:05:41 +0200

    new link

--- MU/TransportModule/Container/LinkContainer.php
+++ MU/TransportModule/Container/LinkContainer.php
@@ -14,10 +14,39 @@ namespace MU\TransportModule\Container;
 
 use MU\TransportModule\Container\Base\AbstractLinkContainer;
 
+use Zikula\Core\LinkContainer\LinkContainerInterface;
+
 /**
  * This is the link container service implementation class.
  */
 class LinkContainer extends AbstractLinkContainer
 {
-    // feel free to add own extensions here
+	/**
+	 * Returns available header links.
+	 *
+	 * @param string $type The type to collect links for
+	 *
+	 * @return array Array of header links
+	 */
+	public function getLinks($type = LinkContainerInterface::TYPE_ADMIN)
+	{
+        $links = parent::getLinks($type = LinkContainerInterface::TYPE_ADMIN);
+        
+        $contextArgs = ['api' => 'linkContainer', 'action' => 'getLinks'];
+        $allowedObjectTypes = $this->controllerHelper->getObjectTypes('api', $contextArgs);
+
+        $permLevel = LinkContainerInterface::TYPE_ADMIN == $type ? ACCESS_ADMIN : ACCESS_READ;
+        $routeArea = LinkContainerInterface::TYPE_ADMIN == $type ? 'admin' : '';
+        
+        if ($routeArea == 'admin' && $this->permissionApi->hasPermission($this->getBundleName() . '::', '::', ACCESS_ADMIN)) {
+        	$links[] = [
+        			'url' => $this->router->generate('mutransportmodule_database_' . $routeArea . 'select2databases'),
+        			'text' => $this->__('Database select', 'mutransportmodule'),
+        			'title' => $this->__('Select 2 databases for transport of datas', 'mutransportmodule'),
+        			'icon' => 'wrench'
+        	];
+        }
+        
+        return $links;
+	}
 }
