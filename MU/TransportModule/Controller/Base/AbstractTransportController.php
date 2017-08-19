e4f3852344284f42d8adfa2cfcf34cc738012f9f
Portugao <info@homepages-mit-zikula.de>
2017-08-19 22:50:27 +0200

    get and insert datas...does not
    work yet

--- MU/TransportModule/Controller/Base/AbstractTransportController.php
+++ MU/TransportModule/Controller/Base/AbstractTransportController.php
@@ -21,6 +21,7 @@ use MU\TransportModule\Entity\TableEntity;
 use MU\TransportModule\Form\Type\DatabaseselectType;
 use MU\TransportModule\Form\Type\TableselectType;
 use MU\TransportModule\Form\Type\FieldselectType;
+use PDO;
 
 /**
  * Table controller base class.
@@ -123,12 +124,37 @@ abstract class AbstractTransportController extends AbstractController
     	if (!$this->hasPermission($this->name . '::', '::', ACCESS_ADMIN)) {
     		throw new AccessDeniedException();
     	}
+    	
+    	// we get factory helper
+    	$entityFactory = $this->get('mu_transport_module.entity_factory');
+    	// we get a table controller
+    	$tableRepository = $entityFactory->getRepository('table');
     
     	$form = $this->createForm(FieldselectType::class);
     
     	if ($form->handleRequest($request)->isValid()) {
     		if ($form->get('select')->isClicked()) {
     			$formData = $form->getData();
+    			// we get the source table id
+    			$sourceId = $formData['source'];
+    			// we get the target table id
+    			$targetId = $formData['target'];
+    			// we get the source table
+    			$sourceTable = $tableRepository->selectById($sourceId);
+    			// we get the target table
+    			$targetTable = $tableRepository->selectById($targetId);
+    			// we get the source database
+    			$sourceDatabase = $sourceTable['database'];
+    			// we get the target database
+    			$targetDatabase = $targetTable['database'];
+    			// we get the source entries
+    			$conn = new \PDO('mysql:dbname=' . $sourceDatabase['dbName'] . ';host=' . $sourceDatabase['host'], $sourceDatabase['dbUser'], $sourceDatabase['dbPassword']);
+    			$result = $conn->query('SELECT * FROM ' . $sourceTable['name']);
+    			while ($row = $result->fetchALL()) {
+    				$sourceEntries[] = $row[0];
+    			}
+    			//die($sourceEntries[0]['field7']); TODO
+    			// we get the field combination string
     			$fieldCombination = $formData['fieldCombination'];
     			$fieldCombination = explode('|',$fieldCombination);
                 $countCombination = count($fieldCombination);
@@ -137,24 +163,58 @@ abstract class AbstractTransportController extends AbstractController
     			$intoValue = '';
     			// we initialize the value value
     			$valueValue = '';
+    			// we get the into string
     			foreach ($fieldCombination as $combination) {
     				$combinationDatas = explode(';', $combination);
     				$target = $combinationDatas[0];
-    				if ($countCombination > $count) {			
+    				if ($countCombination - 1 > $count) {			
     				    $intoValue .= $target . ', ' ;
     				} else {
-    					$intoValue .= $target ;
+    					$intoValue .= $target;
     				}
-    				$sources = explode(',', $combinationDatas[1]);
-    				foreach ($sources as $source) {
-    					$valueValue = $valueValue . $source;
-    				}
-    				
-
+    				$count++;
     			}
-                //die($valueValue);
+    			
+    			$count2 = 0;
+    			$countFieldCombination = count($fieldCombination);    			
+    			foreach ($fieldCombination as $combination) {
+    				$combinationDatas = explode(';', $combination);
+    				$sources = $combinationDatas[1];
+    				$sources = explode(',', $sources);
+    				$countSources = count($sources);
+    				if (is_array($sources)) {
+    					$count1 = 0;
+    				    foreach ($sources as $source) {
+    					    $valueValue .= '$entry["' . $source . '"]';
+    					    if ($countSources - 1 > $count1) {
+    					        $valueValue .= ' . ';
+    					    }
+    					    $count1++;
+    				    }
+    				    } else {
+    					    $valueValue = '$entry["' . $source . '"]';
+    				    }
+    				if ($countFieldCombination - 1 > $count2) {
+    				    $valueValue = $valueValue . ', ';
+    				}
+    				$placeholder = '?';
+    				if($countFieldCombination - 1 > $count2) {
+    					$placeholder .= ',';
+    				}			
+    				$count2++;
+    			}
+    			
+    			$conn2 = new \PDO('mysql:dbname=' . $targetDatabase['dbName'] . ';host=' . $targetDatabase['host'], $targetDatabase['dbUser'], $targetDatabase['dbPassword']);
+    			
+    			foreach ($sourceEntries as $entry) { 			
+    			    $statement2 = $conn2->prepare("INSERT INTO " . $targetTable['name'] . " (" . $intoValue . ") VALUES ($placeholder)");
+    			    $copyState = $statement2->execute(array($valueValue));
+    			    if ($copyState == false) {
+    			    	$this->addFlash('error', $this->__('error'));
+    			    }
+    			}
                
-    			$this->addFlash('status', $this->__('Done! Module configuration updated.'));
+    			$this->addFlash('status', $this->__('Done! The data have been copied from the source table to the target table.'));
     			$userName = $this->get('zikula_users_module.current_user')->get('uname');
     			$this->get('logger')->notice('{app}: User {user} updated the configuration.', ['app' => 'MUTransportModule', 'user' => $userName]);
     		} elseif ($form->get('cancel')->isClicked()) {
@@ -162,7 +222,7 @@ abstract class AbstractTransportController extends AbstractController
     		}
     
     		// redirect to config page again (to show with GET request)
-    		return $this->redirectToRoute('mutransportmodule_transport_select2tables', array('source' => $formData['sourceDatabase'], 'target' => $formData['targetDatabase']));
+    		return $this->redirectToRoute('mutransportmodule_transport_handlefields', array('source' => $formData['source'], 'target' => $formData['target']));
     	}
     
     	$templateParameters = [
@@ -171,11 +231,7 @@ abstract class AbstractTransportController extends AbstractController
     	
     	$source = $request->query->getDigits('source');
     	$target = $request->query->getDigits('target');
-    	
-    	// we get factory helper
-    	$entityFactory = $this->get('mu_transport_module.entity_factory');
-    	// we get a table controller
-    	$tableRepository = $entityFactory->getRepository('table');
+
     	// we get the source table
     	$sourceTable = $tableRepository->selectById($source);
     	// we get the target table
